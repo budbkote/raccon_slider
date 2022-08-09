a1bcd1a269f4a7fae8e87e4302c2dbe2e48a976a
budbkote <108952957+budbkote@users.noreply.github.com>
2022-08-10 01:05:12 +0300

    Первый раз использую js

---
+++ css/js/main.js
@@ -0,0 +1,32 @@
+
+
+const imgl = document.querySelector('.img-left');
+const imgr = document.querySelector('.img-right');
+function imglA(){ 
+    imgl.classList.toggle('img-left-active')
+
+} 
+
+function imgrA(){
+    imgr.classList.toggle('img-right-active')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
